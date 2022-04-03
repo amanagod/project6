6a37ef2e04963f556cc0f457afd6d562721fc1d7
amanagod <94225481+amanagod@users.noreply.github.com>
2022-04-03 07:00:55 +0530

    Update project.js

--- project.js
+++ project.js
@@ -1,14 +1,14 @@
-var a= Math.floor(Math.random()*6);
-var b= Math.floor(Math.random()*6);
+var a= Math.floor(Math.random()*6+1);
+var b= Math.floor(Math.random()*6+1);
 document.querySelectorAll("img")[0].setAttribute("src","dice"+a+".png");
 document.querySelectorAll("img")[1].setAttribute("src","dice"+b+".png");
 
 if(a>b){
-    document.querySelectorAll("h1") .innerHTML("player 1 WIN");
+    document.querySelector("h1") .innerHTML("player 1 WIN");
 }
 if(a<b){
-    document.querySelectorAll("h1") .innerHTML("player 2 WIN");
+    document.querySelector("h1") .innerHTML("player 2 WIN");
 }
 if(a==b){
-    document.querySelectorAll("h1") .innerHTML("Draw");
+    document.querySelector("h1") .innerHTML("Draw");
 }
