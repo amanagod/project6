3680f65818157a7978109ffd15481d3847b6ffa3
amanagod <94225481+amanagod@users.noreply.github.com>
2022-04-03 06:52:16 +0530

    Update project.js

--- project.js
+++ project.js
@@ -1,14 +1,14 @@
-var a= Math .floor(Math.random()*6);
-var b= Math .floor(Math.random()*6);
-document.querySelector("img")[0].setAttribute("src","dice"+a+".png");
-document.querySelector("img")[1].setAttribute("src","dice"+b+".png");
+var a= Math.floor(Math.random()*6);
+var b= Math.floor(Math.random()*6);
+document.querySelectorAll("img")[0].setAttribute("src","dice"+a+".png");
+document.querySelectorAll("img")[1].setAttribute("src","dice"+b+".png");
 
 if(a>b){
-    document.querySelector("h1") .innerHTML("player 1 WIN");
+    document.querySelectorAll("h1") .innerHTML("player 1 WIN");
 }
 if(a<b){
-    document.querySelector("h1") .innerHTML("player 2 WIN");
+    document.querySelectorAll("h1") .innerHTML("player 2 WIN");
 }
 if(a==b){
-    document.querySelector("h1") .innerHTML("Draw");
+    document.querySelectorAll("h1") .innerHTML("Draw");
 }
