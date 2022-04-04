60cedea0f60046d0a46babaa0c8b9003d5de8e96
amanagod <94225481+amanagod@users.noreply.github.com>
2022-04-04 19:44:45 +0530

    Update project.js

--- project.js
+++ project.js
@@ -1,28 +1,24 @@
 
 var randomNumber1 = Math.floor(Math.random() * 6) + 1; //1-6
 
-var randomDiceImage = "dice" + randomNumber1 + ".png"; //dice1.png - dice6.png
 
-var randomImageSource = "" + randomDiceImage; //images/dice1.png - images/dice6.png
 
-var image1 = document.querySelectorAll("img")[0];
-
-image1.setAttribute("src", randomImageSource);
+document.querySelectorAll("img")[0].setAttribute("src", "dice" + randomNumber1 + ".png");
 
 
 var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
-var randomImageSource2 = "dice" + randomNumber2 + ".png";
 
-document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+
+document.querySelectorAll("img")[1].setAttribute("src", "dice" + randomNumber2 + ".png");
 
 
 //If player 1 wins
 if (randomNumber1 > randomNumber2) {
-  document.querySelector("h1").innerHTML = "ðŸš© Play 1 Wins!";
+  document.querySelector("h1").innerHTML = 🚩 Play 1 Wins!";
 }
 else if (randomNumber2 > randomNumber1) {
-  document.querySelector("h1").innerHTML = "Player 2 Wins! ðŸš©";
+  document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
 }
 else {
   document.querySelector("h1").innerHTML = "Draw!";
