a10d743588c0584c2884d9e97dd6302f1e7c1f5a
amanagod <94225481+amanagod@users.noreply.github.com>
2022-04-04 19:16:49 +0530

    Update project.js

--- project.js
+++ project.js
@@ -1,14 +1,14 @@
-var a= Math.floor(Math.random()*6+1);
-var b= Math.floor(Math.random()*6+1);
-document.querySelectorAll("img")[0].setAttribute("src","dice"+a+".png");
-document.querySelectorAll("img")[1].setAttribute("src","dice"+b+".png");
+var a= Math.floor((Math.random()*6)+1);
+var b= Math.floor((Math.random()*6)+1);
+document.document.getElementsByClassName("img1")[0].setAttribute("src","dice"+a+".png");
+document.document.getElementsByClassName("img2")[1].setAttribute("src","dice"+b+".png");
 
 if(a>b){
-    document.querySelector("h1") .innerHTML("player 1 WIN");
+    document.querySelector("h1").innerHTML("player 1 WIN 🥇");
 }
 if(a<b){
-    document.querySelector("h1") .innerHTML("player 2 WIN");
+    document.querySelector("h1").innerHTML("player 2 WIN 🥇");
 }
 if(a==b){
-    document.querySelector("h1") .innerHTML("Draw");
+    document.querySelector("h1").innerHTML("Draw 🤔");
 }
